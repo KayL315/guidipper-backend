69e3c53a513753790040c6cf402b2d8f3365e196
Hushmonday <wang.peiha@northeastern.edu>
2025-12-01 21:54:27 -0800

    Fix the top bar show avatar and username

--- app/routers/user.py
+++ app/routers/user.py
@@ -43,10 +43,12 @@ def login(user_data: UserLogin, db: Session = Depends(get_db)):
         "access_token": access_token, 
         "token_type": "bearer",
         "user": {
-        "id": user.id,
-        "email": user.email,
-        "username": user.username,
-    }}
+            "id": user.id,
+            "email": user.email,
+            "username": user.username,
+            "avatar_url": user.avatar_url,
+        }
+    }
 
 # Get current user info
 from app.dependencies.auth import get_current_user
